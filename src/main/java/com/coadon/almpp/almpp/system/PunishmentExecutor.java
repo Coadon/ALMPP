59c6c403ef09e5cbff680bec28297351778633ba
Coadon <50821025+Coadon@users.noreply.github.com>
2021-10-17 17:51:19 +0800

    Add copyright header to all java source code files

--- src/main/java/com/coadon/almpp/almpp/system/PunishmentExecutor.java
+++ src/main/java/com/coadon/almpp/almpp/system/PunishmentExecutor.java
@@ -1,3 +1,15 @@
+/*
+ * ALMPP - a bukkit plugin
+ * Copyright (C) 2021 Coadon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.coadon.almpp.almpp.system;
 
 import com.coadon.almpp.almpp.ALMPP;
