caad1b77bc3f243a6b6c2c0b4348cc196ecaa41d
Coadon <50821025+Coadon@users.noreply.github.com>
2021-10-31 14:34:02 +0800

    New command /almpp

---
+++ src/main/java/com/coadon/almpp/almpp/commands/Commandalmpp.java
@@ -0,0 +1,40 @@
+/*
+ * ALMPP - The advanced lightweight punishment plugin for Minecraft
+ * Copyright (C) 2021 Coadon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.coadon.almpp.almpp.commands;
+
+import com.coadon.almpp.almpp.ALMPP;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class Commandalmpp extends PluginCommand {
+
+    public Commandalmpp(ALMPP plugin) {
+        super(plugin, "almpp");
+    }
+
+    @Override
+    public void run(@NotNull Server server, @NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull Arguments args) throws Throwable {
+        sender.sendMessage(Component.text("ALMPP ").color(NamedTextColor.DARK_AQUA)
+                .append(Component.text("v" + plugin.getDescription().getVersion() + "\n").color(NamedTextColor.LIGHT_PURPLE)));
+    }
+}
