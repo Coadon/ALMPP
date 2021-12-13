639a92aad2f5f66b7a7e86f9b388937ccef3cfa4
Coadon <50821025+Coadon@users.noreply.github.com>
2021-12-13 17:22:32 +0800

    Implement TabCompletion to PluginCommand.java

--- src/main/java/com/coadon/almpp/almpp/commands/PluginCommand.java
+++ src/main/java/com/coadon/almpp/almpp/commands/PluginCommand.java
@@ -26,21 +26,19 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-public abstract class PluginCommand implements IPluginCommand, CommandExecutor {
+public abstract class PluginCommand implements IPluginCommand, TabExecutor {
     protected final ALMPP plugin;
     protected final Logger logger;
     protected final String cmdLabel;
@@ -68,16 +66,17 @@ public abstract class PluginCommand implements IPluginCommand, CommandExecutor {
     public abstract void run(@NotNull Server server, @NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull Arguments args) throws Throwable;
 
     @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return null;
+    }
+
+    @Override
     public void showError(@NotNull final CommandSender sender, final Throwable throwable) {
         Component output = Component.text()
                 .append(Component.text("An error occurred while performing this command").color(NamedTextColor.RED))
                 .append(Component.text(throwable != null ? (throwable.getMessage() != null ? ":\n" + throwable.getMessage() : ".") : ".").color(NamedTextColor.RED))
                 .build();
         sender.sendMessage(output);
-    }
-
-    public void registerItself() {
-        plugin.registerCommand(cmdLabel, this);
     }
 
     protected @Nullable Player getPlayer(final String playerName) {
@@ -114,19 +113,5 @@ public abstract class PluginCommand implements IPluginCommand, CommandExecutor {
 
     protected IComponentProvider getFormatter() {
         return plugin.getFormatter();
-    }
-
-    /**
-     * Lists all plugin names.
-     *
-     * @param server Server instance
-     * @return List of plugin names
-     */
-    protected final List<String> getPlugins(final Server server) {
-        final List<String> plugins = new ArrayList<>();
-        for (final Plugin p : server.getPluginManager().getPlugins()) {
-            plugins.add(p.getName());
-        }
-        return plugins;
     }
 }
