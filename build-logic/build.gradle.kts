645fdd5741e88ffb71458f2e754b1bf717a3fc8a
Coadon <50821025+Coadon@users.noreply.github.com>
2022-01-26 22:00:19 +0800

    Upgrade gradle build script and improve versioning system

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("net.kyori", "indra-common", "2.0.6")
+    implementation("gradle.plugin.com.github.jengelman.gradle.plugins", "shadow", "7.0.0")
+}
